f40e0984abcc498b19d79b1366619fb19793c284
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-08-18 20:06:55 -0700

    authentication

--- backend/schemas/resolvers.js
+++ backend/schemas/resolvers.js
@@ -1,18 +1,48 @@
-const {Game} = require ('../models') 
-const resolvers = {
-    Query:{
-        games: async () => {
-            return Game.find().sort({ createdAt: -1 });
-          },
-          game: async (parent, { gameId }) => {
-            return Game.findOne({ _id: gameId });
-          },
-    }, 
-    Mutation:{
-        createGame: async (parent, args) => {
-            const game = await Game.create({})
-            console.log(game)
-            return game 
-    }}}
+const { Game, User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
-    module.exports = resolvers 
+const resolvers = {
+  Query: {
+    games: async () => {
+      return Game.find().sort({ createdAt: -1 });
+    },
+    game: async (parent, { gameId }) => {
+      return Game.findOne({ _id: gameId });
+    },
+    me: () => {
+      return {};
+    },
+  },
+  Mutation: {
+    createGame: async (parent, args) => {
+      const game = await Game.create({});
+      console.log(game);
+      return game;
+    },
+    signup: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
--- backend/schemas/typeDefs.js
+++ backend/schemas/typeDefs.js
@@ -7,14 +7,26 @@ type Game {
    xIsNext: Boolean
 }
 
+type User {
+    _id: ID!
+    email: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
 type Query {
     games: [Game], 
     game(gameId:ID!): Game
+    me: User
 }
 
 type Mutation {
     createGame: Game
- 
+    login(email: String!, password: String!): Auth
+    signup(email: String!, password: String!): Auth
 }
 `
    //add update game and more by create game
