16a0df8529f49503d5925fc3c90eafda7ac2e7d9
Crystal Taylor <kittytaylor@gmail.com>
2021-08-25 15:48:33 -0700

    fixing server.js

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -50,9 +50,6 @@ function App() {
           <Route exact path="/game">
             <Game />
           </Route>
-          <Route exact path="/leaderboard">
-            <LeaderBoard />
-          </Route>
         </>
       </Router>
     </ApolloProvider>
